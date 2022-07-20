3a4ed302f59051ce86cc183dab2facbb3168a624
farhan ali <farhan.ali@rockvillegroup.om>
2022-07-20 14:32:07 +0500

    update bash

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@
 sudo apt install docker.io -y 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker compose up -d 
+sudo docker-compose up -d 
